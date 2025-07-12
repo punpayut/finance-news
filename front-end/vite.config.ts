6bd4373996cc4fbec68c1c4eb24e07a364330760
Payut Charoensri <punpayut@gmail.com>
2025-07-13 04:06:42 +0700

    Update vite.config.ts

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Re-add rewrite for local development
+        
       },
     },
   },
